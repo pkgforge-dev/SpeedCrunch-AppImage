170a9cd75ee60166a869d3488a818de942d26866
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-11-23 04:36:40 -0400

    refactor

--- speedcrunch-appimage.sh
+++ speedcrunch-appimage.sh
@@ -1,47 +1,59 @@
 #!/bin/sh
-set -u
-APP=speedcrunch
-SITE="heldercorreia/speedcrunch"
+
+set -eu
+
 export ARCH="$(uname -m)"
-APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
 export APPIMAGE_EXTRACT_AND_RUN=1
 
-# CREATE DIRECTORIES
-[ -n "$APP" ] && mkdir -p "./$APP/tmp" && cd "./$APP/tmp" || exit 1
+APP=SpeedCrunch
+SITE="heldercorreia/speedcrunch"
+UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|continuous|*$ARCH.AppImage.zsync"
+APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
 
-# DOWNLOAD AND EXTRACT THE ARCHIVE
-version=$(curl -Ls https://api.bitbucket.org/2.0/repositories/"$SITE"/downloads | sed 's/[()",{} ]/\n/g' | grep -o 'https.*SpeedCrunch.*64.*bz2$' | head -1)
-wget "$version" && tar fx ./*tar* && rm -f ./*tar* || exit 1
-cd ..
-mkdir -p "./$APP.AppDir/usr/bin" && mv ./tmp/* "./$APP.AppDir/usr/bin" || exit 1
-cd "./$APP.AppDir" || exit 1
-
-# DESKTOP ENTRY AND ICON
 DESKTOP="https://bitbucket.org/heldercorreia/speedcrunch/raw/fa4f5d23f28b6458b54c617230f66af41fc94d7e/pkg/org.speedcrunch.SpeedCrunch.desktop"
 ICON="https://bitbucket.org/heldercorreia/speedcrunch/raw/fa4f5d23f28b6458b54c617230f66af41fc94d7e/gfx/speedcrunch.svg"
-wget $DESKTOP -O ./$APP.desktop && wget $ICON -O ./org.speedcrunch.SpeedCrunch.png && ln -s ./$APP.png ./.DirIcon
+
+# CREATE DIRECTORIES
+mkdir -p "./$APP/tmp"
+cd "./$APP/tmp"
+
+# DOWNLOAD AND EXTRACT THE ARCHIVE
+APP_URL=$(curl -Ls https://api.bitbucket.org/2.0/repositories/"$SITE"/downloads \
+  | sed 's/[()",{} ]/\n/g' | grep -o 'https.*SpeedCrunch.*64.*bz2$' | head -1)
+wget "$APP_URL"
+tar fx ./*.tar.*
+rm -f ./*.tar.*
+cd ..
+mkdir -p ./AppDir/usr/bin
+mv ./tmp/* ./AppDir/usr/bin
+cd ./AppDir
+
+# DESKTOP ENTRY AND ICON
+wget $DESKTOP -O ./$APP.desktop
+wget $ICON -O ./org.speedcrunch.SpeedCrunch.png
+ln -s ./org.speedcrunch.SpeedCrunch.png ./.DirIcon
 
 export VERSION="$(echo "$version" | awk -F"-" '{print $(NF-1)}')"
 
 # AppRun
 cat >> ./AppRun << 'EOF'
-#!/bin/sh
+#!/usr/bin/env sh
 CURRENTDIR="$(readlink -f "$(dirname "$0")")"
 export GCONV_PATH="$CURRENTDIR/usr/lib/gconv"
 exec "$CURRENTDIR/ld-linux-x86-64.so.2" \
 	--library-path "$CURRENTDIR/usr/lib" \
 	"$CURRENTDIR"/usr/bin/speedcrunch "$@"
 EOF
-chmod a+x ./AppRun
+chmod +x ./AppRun
 
 # BUNDLE ALL LIBS
 mkdir -p ./usr/lib
-ldd ./usr/bin/speedcrunch | awk -F"[> ]" '{print $4}' | xargs -I {} cp -f {} ./usr/lib
-mv ./usr/lib/ld-linux-x86-64.so.2 ./
+ldd ./usr/bin/speedcrunch | awk -F"[> ]" '{print $4}' | xargs -I {} cp -vf {} ./usr/lib
+mv ./usr/lib/ld-linux-x86-64.so.2 ./ || true
 if [ ! -f ./ld-linux-x86-64.so.2 ]; then
-  cp /lib64/ld-linux-x86-64.so.2 ./ || exit 1
+  cp /lib64/ld-linux-x86-64.so.2 ./
 fi
-cp -r /usr/lib/gconv ./usr/lib/gconv || exit 1
+cp -rv /usr/lib/gconv ./usr/lib/gconv
 
 find ./usr/lib ./usr/bin -type f -exec strip -s -R .comment --strip-unneeded {} ';'
 
@@ -51,8 +63,10 @@ wget -q "$APPIMAGETOOL" -O ./appimagetool
 chmod +x ./appimagetool
 
 # Do the thing!
-./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 10 \
-  -u "gh-releases-zsync|Samueru-sama|SpeedCrunch-AppImage|continuous|*x86_64.AppImage.zsync" \
-  ./"$APP".AppDir SpeedCrunch-"$VERSION"-"$ARCH".AppImage 
-[ -n "$APP" ] && mv ./*.AppImage* .. && cd .. && rm -rf ./"$APP" || exit 1
+./appimagetool --comp zstd \
+  --mksquashfs-opt -Xcompression-level --mksquashfs-opt 10 \
+  -n -u "$UPINFO" "$PWD"/"$APP".AppDir "$PWD"/"$APP"-"$VERSION"-anylinux-"$ARCH".AppImage
+mv ./*.AppImage* ..
+cd ..
+rm -rf ./"$APP" || exit 1
 echo "All Done!"
